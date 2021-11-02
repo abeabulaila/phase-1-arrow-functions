f09ea212466ce908458ac1b0f869adfc2c6f8a27
Ibrahim Abulaila <abeabulaila@gmail.com>
2021-11-02 09:48:29 -0400

    commit tree 20d480608d88b89a53edfb3a01f99e8324e19453

---
+++ index.js
@@ -0,0 +1,12 @@
+function divide (num1, num2){
+   num2 = 2000
+    num1 = 100
+    return (num2/num1);  
+} 
+
+const square = times => times * times;
+
+const add = (sum1, sum2) => sum1 + sum2 
+
+add(3, 4)
+
